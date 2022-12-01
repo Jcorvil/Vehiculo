48811f2b4bfc1b19890aeaa7c49ab08bcbd96623
Jcorvil <jcorvil600@g.educaand.es>
2022-12-01 11:29:27 +0100

    .

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,7 @@
 public class Principal {
     public static void main(String[]args) {
-        Vehiculo v1 = new Vehiculo();
-        v1.setMatricula("8888AB");
-
         Coche c1 = new Coche();
+        c1.setMatricula("8888AB");
         c1.setMarca("Honda");
         c1.setModelo("Civic");
         c1.setAntiguedad(5);
@@ -15,7 +13,7 @@ public class Principal {
         p1.setEdad(25);
 
         System.out.println("Información del vehículo: ");
-        System.out.println("Matrícula: " + v1.getMatricula());
+        System.out.println("Matrícula: " + c1.getMatricula());
         System.out.println("Marca, modelo y antigüedad: " + c1.getMarca() + " " + c1.getModelo() + " // " + c1.getAntiguedad() + " años");
         System.out.println("Nombre, apellidos, DNI y edad del propietario: ");
         System.out.println(p1.getNombre() + " " + p1.getApellidos() + " // " + p1.getDNI() + " // " + p1.getEdad() + " años");
--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -3,13 +3,6 @@ public class Vehiculo {
     private static Propietario Propietario;
     private static String matricula;
 
-    public static void main(String[] args){
-        Vehiculo Vehiculo1 = new Vehiculo();
-        System.out.println("Vehiculo 1");
-        Vehiculo1.setMatricula("12345A");
-        System.out.println("Numero de matricula: " + Vehiculo1.getMatricula());
-    }
-
     public String getMatricula() {
         return matricula;
     }
